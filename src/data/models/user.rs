fdadae54e1229fba241a8f16e5cb090d69b6f434
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2024-10-28 15:57:23 +0530

    Update user.rs

--- src/data/models/user.rs
+++ src/data/models/user.rs
@@ -4,7 +4,6 @@ use surrealdb::sql::Thing;
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct User {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<Thing>,
     pub name: String,
     pub email: String,
